abae983f07ba6820996be5517b1fbd45697ad54e
Jake Quinter <hello@jakequinter.io>
2022-04-30 15:56:49 -0400

    app container and navigation layout

---
+++ src/components/Container.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import { ArrowRight } from 'iconoir-react';
+import { motion } from 'framer-motion';
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const iconVariants = {
+  hover: {
+    x: 2,
+  },
+};
+
+export default function Container({ children }: Props) {
+  const { data: session } = useSession();
+
+  return (
+    <div className="bg-slate-50 min-h-screen">
+      <nav className="flex items-center justify-between p-4 max-w-screen-lg mx-auto">
+        <Link href="/" passHref>
+          <a className="text-2xl font-bold text-slate-900">tin</a>
+        </Link>
+
+        <div className="flex space-x-8 items-center">
+          {!session && (
+            <button
+              className="font-medium text-slate-700"
+              onClick={() => signIn('google')}
+            >
+              Sign in
+            </button>
+          )}
+
+          <motion.button
+            whileHover="hover"
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-slate-300 shadow-sm text-sm font-medium rounded-md text-slate-700  hover:border-slate-400 focus:outline-none focus:ring-0"
+            onClick={() => signOut()}
+          >
+            {session ? 'Dashboard' : 'Sign up'}
+            <motion.span variants={iconVariants}>
+              <ArrowRight className="ml-1" />
+            </motion.span>
+          </motion.button>
+        </div>
+      </nav>
+
+      <main>{children}</main>
+    </div>
+  );
+}
