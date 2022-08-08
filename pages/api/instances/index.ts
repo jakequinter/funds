345c3fc05fa5fcfd6a9489094dd1b6f275b30aaa
Jake Quinter <hello@jakequinter.io>
2022-08-08 19:50:55 -0400

    try/catch in instances api call

--- pages/api/instances/index.ts
+++ pages/api/instances/index.ts
@@ -8,17 +8,21 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let instances: DocumentData = [];
-   const instancesRef = db.collection('instances');
-   const snapshot = await instancesRef.get();
-
-   if (snapshot.empty) {
-     return res.status(404).json({ error: 'No instances found' });
-   }
-
-   snapshot.forEach(doc => {
-      instances.push({ id: doc.id, ...doc.data() });
-   });
-
-    return res.status(200).json(instances);
+  try {
+    let instances: DocumentData = [];
+    const instancesRef = db.collection('instances');
+    const snapshot = await instancesRef.get();
+ 
+    if (snapshot.empty) {
+      return res.status(404).json({ error: 'No instances found' });
+    }
+ 
+    snapshot.forEach(doc => {
+       instances.push({ id: doc.id, ...doc.data() });
+    });
+ 
+     return res.status(200).json(instances); 
+  } catch (error) {
+    return res.status(500).json({ error: error });
+  }
 }
