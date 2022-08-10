5dd1f9673aad8434e142cc21f781c313611335fe
Jake Quinter <hello@jakequinter.io>
2022-08-10 17:59:51 -0400

    remove userId

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import { useAuth } from '@/hooks/useAuth';
 import Container from '@/components/Container';
 
 const Home: NextPage = () => {
-  const { user } = useAuth();
-
   return (
     <Container>
       <Head>
         <title>funds</title>
       </Head>
-
-      <h1>{user?.uid}</h1>
     </Container>
   );
 };
