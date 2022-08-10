4706ef4ab48fb7315c8b9b9cba4fe66bd62def5c
Jake Quinter <hello@jakequinter.io>
2022-08-10 09:40:32 -0400

    remove app id

--- src/lib/firebase/initFirebase.ts
+++ src/lib/firebase/initFirebase.ts
@@ -7,7 +7,7 @@ const config = {
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
