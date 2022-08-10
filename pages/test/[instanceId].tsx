463810f0671c197ce0cb6ed0fdb40f0527697b86
Jake Quinter <hello@jakequinter.io>
2022-08-10 09:26:07 -0400

    test SSG

---
+++ pages/test/[instanceId].tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { getInstance } from '@/lib/firebaseFunctions/getInstances';
+import { Instance } from '@/types/instance';
+
+type Props = {
+  instanceId: string;
+  instance: Instance;
+};
+
+export default function SSRPage({ instanceId, instance }: Props) {
+  console.log('instance', instanceId);
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js w/ Firebase Client-Side</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Next.js w/ Firebase Server-Side</h1>
+        <h1>{instanceId}</h1>
+        <h2>{instance.month}</h2>
+        <p>{instance.year}</p>
+      </main>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const instanceId = context.params?.instanceId as string;
+
+  const instance = await getInstance('aZdlTmziQI86o7zNBmxq');
+  if (!instance) {
+    return { notFound: true };
+  }
+  return {
+    props: {
+      instanceId,
+      instance,
+    },
+  };
+};
